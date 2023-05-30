138b81bdaa99f47fe4d9a996fd8e87f3a4066c43
jorovernier <jorovernier@gmail.com>
2023-05-30 11:46:32 -0700

    added code

--- testgoogle/practiceTest.test.js
+++ testgoogle/practiceTest.test.js
@@ -5,6 +5,7 @@ let driver;
 // Build a new driver for each test
 beforeEach(async () => {
   driver = await new Builder().forBrowser(Browser.CHROME).build();
+  await driver.get("https://www.google.com/");
 });
 
 // Quit a driver after each test
@@ -15,7 +16,7 @@ afterEach(async () => {
 describe("Test the Google homepage", () => {
   test("can search Google for 'Selenium'", async () => {
     // Navigate to google.com
-    await driver.get("https://www.google.com/");
+    // await driver.get("https://www.google.com/");
 
     // Locate the search bar and send the search term to it
     await driver.findElement(By.name("q")).sendKeys("selenium", Key.RETURN);
@@ -24,14 +25,13 @@ describe("Test the Google homepage", () => {
   });
 
   test("can search Google and go to images tab", async () => {
-    await driver.get("https://www.google.com/");
-
     await driver.findElement(By.name("q")).sendKeys('typhlosion', Key.RETURN);
+    await driver.sleep(2000)
 
     await driver.wait(until.titleIs('typhlosion - Google Search'), 1000);
+    await driver.sleep(2000)
 
     await driver.findElement(By.linkText('Images')).click();
-
     await driver.sleep(2000)
 
     const currentPage = await driver.wait(until.elementLocated(By.css('span[aria-current="page"]')), 1000)
